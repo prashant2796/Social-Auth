74f601cc71af2a49d03c982b53eca3e349ea5d32
prashant <prashantpandey071@gmail.com>
2018-02-17 02:58:38 +0530

    api call to google working,can access user info now

--- oauth.py
+++ oauth.py
@@ -1,77 +1,111 @@
+from flask import Flask, abort, request
 from urllib.request import urlopen
 from urllib.parse import urlencode,parse_qsl,urlparse
 import webbrowser
 import requests
 
-import requests.auth
-
-# https://www.googleapis.com/auth/userinfo.email
-
 CLIENT_ID='544125358258-jsnghl0v7lljccjpce93hpaj8hiuc5p6.apps.googleusercontent.com'
 
 CLIENT_SECRET='gmNJI8gsjpY_AdbfEkwxajRd'
 
-AUTHORIZE_URL='https://accounts.google.com/o/oauth2/auth'
+AUTHORIZE_URL='https://accounts.google.com/o/oauth2/auth?'
 
-CALLBACK_URL='http://127.0.0.1:8000/admin/oauth2callback'
+CALLBACK_URL='http://127.0.0.1:5000/oauth2callback'
 
 ACCESS_TOKEN_URL='https://www.googleapis.com/oauth2/v3/token'
 
 API_RESOURCE_URL='https://www.googleapis.com/oauth2/v1/userinfo'
 
+SCOPE_URL='https://www.googleapis.com/auth/user.emails.read'
+
+
+app = Flask(__name__)
+@app.route('/')
+
+def homepage():
+	"""
+	Displays the authorization link to users.
+
+	"""
+	text = '<a href="%s">Login with google</a>'
+	return text % get_authorization_url()
+
+
 def get_authorization_url():
 
+	"""
+	this function returns the authorization url
+
+	it makes a get request to the authorization 
+	url of google and  with the parameters
+	that are appended to it.
+	"""
+
 	auth_parameters= {"client_id":CLIENT_ID,
-					  "response_type":"code",
 					  "redirect_uri":CALLBACK_URL,
-					  "scope":"email"
+					  "scope":SCOPE_URL,	
+  					  "response_type":"code",
+					  "access_type":'offline',
+   					  "include_granted_scopes":'true'				  
 					 }
+	auth_url=requests.get(AUTHORIZE_URL,params=auth_parameters) 
+	#makes a get request to authorization url
 
-	auth_url="?".join([AUTHORIZE_URL,urlencode(auth_parameters)])
-	webbrowser.open_new_tab(auth_url)
+	return auth_url.url   #the .url method returns the url with parameters appended to it.
 
-get_authorization_url()
+@app.route('/oauth2callback') 
+def google_call_back():
+	"""
+	The control is switched to this function when user is redirected after authorizing the client app.
 
+	It returns the final user data which is defined in scope parameter.
 
-redirect_url = input("Paste here url you were redirected:\n")
+	"""
+	error = request.args.get('error', '')     
+	if error:
+	    return "Error: " + error        #returns the error if there is one.
 
+	auth_code = request.args.get('code')  #it takes in authorization code
+	access_token = get_access_token(auth_code) #takes in access token returned my the function
+	return "your google info is:%s" % get_user_info(access_token)
 
-def get_authorization_code():
+def get_access_token(auth_code):
+	"""
+	this function makes a post request which exchanges authorization code for 
+	access token and returns it.
 
-	redirect_parameters=dict(parse_qsl(urlparse(redirect_url).query))
-	auth_code=redirect_parameters['code']
-	return auth_code
-
-get_authorization_code()
-
-def get_access_token():
+	"""
 	access_token_parameters={
 								"client_id":CLIENT_ID,
-								"CLIENT_SECRET":CLIENT_SECRET,
+								"client_secret":CLIENT_SECRET,
 								"redirect_uri":CALLBACK_URL,
-								"code":get_authorization_code(),
-								"grant_type":'authorization_code'
+								"code":auth_code,
+								"grant_type":"authorization_code"
 							}
-	result=urlopen(ACCESS_TOKEN_URL,data=urlencode(access_token_parameters).encode("utf-8"))
-	result_content=json.loads(result.read())
-	print(result_content)
-	# access_token=result_content['access_token']
-	# print(access_token)
-
-	# client_auth = requests.auth.HTTPBasicAuth(CLIENT_ID, CLIENT_SECRET)
-	# post_data = {"grant_type": "authorization_code",
-	#              "code": get_authorization_code(),
-	#              "redirect_uri": CALLBACK_URL}
-	# response = requests.post(AUTHORIZE_URL,
-	#                          auth=client_auth,
-	#                          data=post_data)
-	# token_json = response.json()
-	# my_access_token=token_json["access_token"]
-	# print(my_access_token)
 
 	
+	token_result=requests.post(ACCESS_TOKEN_URL,params=access_token_parameters)
+	access_token_result = token_result.json()    #stores the json form of the result 
+	return access_token_result["access_token"]
 
-get_access_token()
+
+def get_user_info(access_token):
+	"""
+	this function makes api calls to google resource server
+
+	it exchanges access token to get user_info
+
+	"""
+	resource_parameters={
+
+						"access_token":access_token,
+						"Content-Type":"application/json"
+
+					}
+	user_info=requests.get(API_RESOURCE_URL, params=resource_parameters)
+	my_user_info=user_info.json()
+	return my_user_info
 
 
-
+if __name__ == '__main__':
+    app.run(debug=True)
