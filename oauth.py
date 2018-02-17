225cf9a145626764d34001529ed45fc2af380b59
prashant <prashantpandey071@gmail.com>
2018-02-17 16:48:45 +0530

    added all the constant variables to a new file config.py

--- oauth.py
+++ oauth.py
@@ -12,23 +12,15 @@ The access token then can be send to resource server to get back the user inform
 """
 
 
-
+from config import (CLIENT_ID,
+					CLIENT_SECRET,
+					AUTHORIZE_URL,
+					CALLBACK_URL,
+					ACCESS_TOKEN_URL,
+					API_RESOURCE_URL,
+					SCOPE_URL)
 from flask import Flask, abort, request
 import requests
-
-CLIENT_ID='544125358258-jsnghl0v7lljccjpce93hpaj8hiuc5p6.apps.googleusercontent.com' #the client_id which you get from google api console
-
-CLIENT_SECRET='gmNJI8gsjpY_AdbfEkwxajRd' #the client secret which you get from api console
-
-AUTHORIZE_URL='https://accounts.google.com/o/oauth2/auth?' #get request to this url gives back the authorization code
-
-CALLBACK_URL='http://127.0.0.1:5000/oauth2callback' #this is the url of your website.The user is redirected to this url after the user has authorize your app. 
-
-ACCESS_TOKEN_URL='https://www.googleapis.com/oauth2/v3/token' #get or post request to this url gives back the access token
-
-API_RESOURCE_URL='https://www.googleapis.com/oauth2/v1/userinfo' #get request to this url gives back the desired user info.
-
-SCOPE_URL='https://www.googleapis.com/auth/user.emails.read' #since we need users profile information 
 
 
 app = Flask(__name__)
